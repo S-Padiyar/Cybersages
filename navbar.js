8949014b354491d6302a3d0e7e36cea6d1c9622a
setokaiba789 <sunmayp@gmail.com>
2023-07-25 18:15:22 -0400

    fixed some stuff, made code cleaner

--- navbar.js
+++ navbar.js
@@ -1,28 +1,33 @@
-const Shadow = document.getElementsByClassName("navbar");
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
-function toggle(){
-  if(window.scrollY == 0){
+Array.from(navLinks).forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
+
+function toggle() {
+  const Shadow = document.querySelectorAll(".navbar");
+  
+  if (window.scrollY === 0) {
     for (let i = 0; i < Shadow.length; i++) {
       Shadow[i].style.boxShadow = "none";
     }
-  }
-  else{
+  } else {
     for (let i = 0; i < Shadow.length; i++) {
       Shadow[i].style.boxShadow = "0px 2px 5px rgba(55, 124, 145, 0.5)";
     }
   }
 }
-window.onscroll = toggle;     
 
+window.onscroll = toggle;
 
